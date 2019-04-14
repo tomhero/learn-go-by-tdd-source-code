3c4a6758f38d893fd46b34f80bf7bd7fc826d004
Chayut <chayut.191@gmail.com>
2019-04-14 19:31:35 +0700

    Finish simple Sum function for slice type

--- 04_array/sum.go
+++ 04_array/sum.go
@@ -1,10 +1,10 @@
 package array
 
 // Sum function resuturn summy of array
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	summy := 0
-	for i := 0; i < 5; i++ {
-		summy += numbers[i]
+	for _, number := range numbers {
+		summy += number
 	}
 	return summy
 }
--- 04_array/sum_test.go
+++ 04_array/sum_test.go
@@ -3,12 +3,25 @@ package array
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("sum of five number", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if want != got {
-		t.Errorf("want '%d' got '%d' give, %v", want, got, numbers)
-	}
+		if want != got {
+			t.Errorf("want '%d' got '%d' give, %v", want, got, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if want != got {
+			t.Errorf("want %d got %d from, %v", want, got, numbers)
+		}
+	})
 }
