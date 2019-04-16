19fe791e97ad877ee5e62ac3e1da05675a22ede2
Chayut <chayut.191@gmail.com>
2019-04-16 21:05:02 +0700

    Finish Array and slice section!!

--- 04_array/sum.go
+++ 04_array/sum.go
@@ -9,14 +9,16 @@ func Sum(numbers []int) int {
 	return summy
 }
 
-// SumAll can return sum of number of slices??
-func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+// SumAllTails can return sum of number of slices??
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
-	return sums
+	return
 }
--- 04_array/sum_test.go
+++ 04_array/sum_test.go
@@ -29,15 +29,26 @@ func TestSum(t *testing.T) {
 	})
 }
 
-func TestSumAll(t *testing.T) {
-	t.Run("test adding positive value", func(t *testing.T) {
-		got := SumAll([]int{1, 2}, []int{3, 9})
-		want := []int{3, 12}
-		// want := "bob"
+func TestSumAllTails(t *testing.T) {
 
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
 		// check if two slices are equal or not??
 		if !reflect.DeepEqual(got, want) {
-			t.Errorf("want %v got %v", want, got)
+			t.Errorf("got %v want %v", got, want)
 		}
+	}
+
+	t.Run("test adding positive value", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{3, 9})
+		want := []int{2, 9}
+		// want := "bob"
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
 	})
 }
